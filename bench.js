7023725b4f389ae2c069676f4da1e3b70607a7cf
Vsevolod Strukchinsky <floatdrop@gmail.com>
2016-11-16 12:19:50 +0500

    add benchmark

---
+++ bench.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const Benchmark = require('benchmark');
+const PQueue = require('./');
+
+const suite = new Benchmark.Suite();
+
+suite
+	.add('baseline', {
+		defer: true,
+		fn(deferred) {
+			const queue = new PQueue();
+			for (let i = 0; i < 100; i++) {
+				queue.add(() => Promise.resolve());
+			}
+			queue.onEmpty().then(() => deferred.resolve());
+		}
+	})
+	.add('operation with random priority', {
+		defer: true,
+		fn(deferred) {
+			const queue = new PQueue();
+			for (let i = 0; i < 100; i++) {
+				queue.add(() => Promise.resolve(), {
+					priority: Math.random() * 100 | 0
+				});
+			}
+			queue.onEmpty().then(() => deferred.resolve());
+		}
+	})
+	.add('operation with increasing priority', {
+		defer: true,
+		fn(deferred) {
+			const queue = new PQueue();
+			for (let i = 0; i < 100; i++) {
+				queue.add(() => Promise.resolve(), {
+					priority: i
+				});
+			}
+			queue.onEmpty().then(() => deferred.resolve());
+		}
+	})
+	.on('cycle', event => {
+		console.log(String(event.target));
+	})
+	.on('complete', function () {
+		console.log('Fastest is ' + this.filter('fastest').map('name'));
+	})
+.run({
+	async: true
+});
