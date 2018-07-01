50fab6e91b263e38fbe9267222c1d8e6d2409bc3
Szymon Marczak <36894700+szmarczak@users.noreply.github.com>
2018-07-01 13:04:17 +0200

    Add @Rafael09ED's tests

--- test.js
+++ test.js
@@ -261,3 +261,100 @@ test('should resolve empty when size is zero', async t => {
 	await queue.onIdle();
 });
 
+test('.add() - throttled', async t => {
+	const result = [];
+	const queue = new PQueue({
+		intervalCap: 1,
+		intervalLength: 500,
+		autoStart: false
+	});
+	queue.add(() => result.push(1));
+	queue.start();
+	await delay(250);
+	queue.add(() => result.push(2));
+	t.deepEqual(result, [1]);
+	await delay(250);
+	t.deepEqual(result, [1, 2]);
+});
+
+test('.add() - throttled, moveToNextIntervalOnDelay false', async t => {
+	const result = [];
+	const queue = new PQueue({
+		intervalCap: 1,
+		moveToNextIntervalOnDelay: false,
+		intervalLength: 500,
+		autoStart: false
+	});
+	const values = [0, 1];
+	values.forEach(value => queue.add(() => delay(600).then(() => result.push(value))));
+	queue.start();
+	delay(550).then(() => {
+		t.is(queue.pending, 2);
+		t.deepEqual(result, []);
+	});
+	delay(650).then(() => {
+		t.is(queue.pending, 1);
+		t.deepEqual(result, [0]);
+	});
+	await delay(1250);
+	t.deepEqual(result, values);
+});
+
+test('.add() - throttled, moveToNextIntervalOnDelay true', async t => {
+	const result = [];
+	const queue = new PQueue({
+		moveToNextIntervalOnDelay: true,
+		intervalCap: 1,
+		intervalLength: 500,
+		autoStart: false
+	});
+	const values = [0, 1];
+	values.forEach(value => queue.add(() => delay(600).then(() => result.push(value))));
+	queue.start();
+	delay(100).then(() => {
+		t.deepEqual(result, []);
+		t.is(queue.pending, 1);
+	});
+	delay(550).then(() => {
+		t.deepEqual(result, []);
+		t.is(queue.pending, 1);
+	});
+	delay(650).then(() => {
+		t.deepEqual(result, [0]);
+		t.is(queue.pending, 0);
+	});
+	delay(1550).then(() => {
+		t.deepEqual(result, [0]);
+	});
+	await delay(1650);
+	t.deepEqual(result, [0, 1]);
+});
+
+test('.add() - throttled 10, concurrency 5', async t => {
+	const result = [];
+	const queue = new PQueue({
+		concurrency: 5,
+		intervalCap: 10,
+		intervalLength: 1000,
+		autoStart: false
+	});
+	const firstV = [...new Array(5).keys()];
+	const secondV = [...new Array(10).keys()];
+	const thirdV = [...new Array(13).keys()];
+	thirdV.forEach(value => queue.add(() => delay(300).then(() => result.push(value))));
+	queue.start();
+	t.deepEqual(result, []);
+	delay(400).then(() => {
+		t.deepEqual(result, firstV);
+		t.is(queue.pending, 5);
+	});
+	delay(700).then(() => {
+		t.deepEqual(result, secondV);
+	});
+	delay(1200).then(() => {
+		t.is(queue.pending, 3);
+		t.deepEqual(result, secondV);
+	});
+	await delay(1400);
+	t.deepEqual(result, thirdV);
+});
