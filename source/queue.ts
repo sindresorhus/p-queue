708b42d2b04d4f73ebe64c0101f184313e34097d
Christian Ziegler <ziegler.christian@kabelmail.de>
2020-04-09 02:53:18 +0200

    add resolve correctly to source

--- source/queue.ts
+++ source/queue.ts
@@ -4,5 +4,5 @@ export interface Queue<Element, Options> {
 	size: number;
 	filter(options: Partial<Options>): Element[];
 	dequeue(): Element | undefined;
-	enqueue(run: Element, options?: Partial<Options>): void;
+	enqueue(run: Element, options?: Partial<Options>, resolve?: Partial<Options>): void;
 }
