6f6480d17f41215166a674af84c2ff9041e3b70f
Alex Kondratyuk <lynxtaa@gmail.com>
2021-03-27 23:09:57 +0300

    fix tests

--- scripts/add-esm-wrapper.ts
+++ scripts/add-esm-wrapper.ts
@@ -1,14 +1,14 @@
 // Adding ESM-wrapper to avoid dual package hazard:
 // https://nodejs.org/api/packages.html#packages_dual_package_hazard
 
-import { writeFileSync } from "fs";
+import {writeFileSync} from 'fs';
 
 const wrapper = `
-import PQueue from "./index.js"
+import PQueue from './index.js'
 export default PQueue.default
 `;
 
-const wrapperPath = "./dist/wrapper.mjs";
+const wrapperPath = './dist/wrapper.mjs';
 
 writeFileSync(wrapperPath, wrapper);
 
